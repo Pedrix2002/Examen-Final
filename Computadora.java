c77e8d07c5a40c22b2624ec6e766503987b5c0de
Pedrix2002 <pdeleonc5@miumg.edu.gt>
2024-11-02 07:48:44 -0600

    Create Computadora.java

---
+++ Computadora.java
@@ -0,0 +1,9 @@
+public class Computadora {
+    private int id;
+    private String nombre;
+    private double precio;
+    private int cantidad;
+    private String marca;
+
+    // Constructor, getters y setters
+}
